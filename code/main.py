2dc0776e0620b91dca81191bad8d6ec4eeea8b46
InathiGcingca <160378332+InathiGcingca@users.noreply.github.com>
2024-02-18 07:36:40 +0200

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,211 @@
+import random
+from typing import List, Tuple
+import pygame
+import sys
+import time
+from sprites import Ball, Block, GameObject, Player, Projectile, Upgrade
+from settings import (
+    BLOCK_HEIGHT,
+    BLOCK_MAP,
+    BLOCK_WIDTH,
+    GAP_SIZE,
+    TOP_OFFSET,
+    UPGRADES,
+    WINDOW_WIDTH,
+    WINDOW_HEIGHT,
+    FRAMERATE,
+)
+
+
+class Game:
+    def __init__(self):
+
+        # setup
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Breakout")
+        self.clock = pygame.time.Clock()
+
+        # background
+        self.bg = self.create_bg()
+
+        # sprites group
+        self.all_sprites = pygame.sprite.Group()
+        self.block_sprites = pygame.sprite.Group()
+        self.upgrade_sprites = pygame.sprite.Group()
+        self.projectile_sprites = pygame.sprite.Group()
+
+        # sprites
+        self.player = Player([self.all_sprites])
+        self.stage_setup()
+        self.ball = Ball([self.all_sprites], self.player, self.block_sprites)
+
+        # hearts
+        self.heart_surface = pygame.image.load(
+            "graphics/other/heart.png"
+        ).convert_alpha()
+
+        # projectile
+        self.projectile_surf = pygame.image.load(
+            "graphics/other/projectile.png"
+        ).convert_alpha()
+        self.can_shoot = True
+        self.shoot_time = 0
+
+        # crt
+        self.crt = CRT()
+
+        # sounds
+        self.powerup_sound = pygame.mixer.Sound("sounds/powerup.wav")
+        self.powerup_sound.set_volume(0.1)
+
+        self.laser_sound = pygame.mixer.Sound("sounds/laser.wav")
+        self.laser_sound.set_volume(0.1)
+
+        self.laserhit_sound = pygame.mixer.Sound("sounds/laser_hit.wav")
+        self.laserhit_sound.set_volume(0.1)
+
+        self.music = pygame.mixer.Sound("sounds/music.wav")
+        self.music.set_volume(0.1)
+        self.music.play(loops=-1)
+
+    def laser_timer(self):
+        if pygame.time.get_ticks() - self.shoot_time >= 500:
+            self.can_shoot = True
+
+    def create_projectile(self):
+        self.laser_sound.play()
+        for projectile in self.player.laser_rects:
+            Projectile(
+                [self.all_sprites, self.projectile_sprites],
+                projectile.midtop - pygame.math.Vector2(0, 0),
+                self.projectile_surf,
+            )
+
+    def projectile_collisions(self):
+        for projectile in self.projectile_sprites:
+            overlap_sprites: List[GameObject] = pygame.sprite.spritecollide(
+                projectile, self.block_sprites, False
+            )
+            if overlap_sprites:
+                for sprite in overlap_sprites:
+                    sprite.get_damage(1)
+                projectile.kill()
+                self.laserhit_sound.play()
+
+    def create_upgrade(self, pos: Tuple[int, int]):
+        upgrade_type = random.choice(UPGRADES)
+        Upgrade([self.all_sprites, self.upgrade_sprites], upgrade_type, pos)
+
+    def upgrade_collisions(self):
+        overlap_sprites: List[Upgrade] = pygame.sprite.spritecollide(
+            self.player, self.upgrade_sprites, True
+        )
+        for sprite in overlap_sprites:
+            self.player.upgrade(sprite.upgrade_type)
+            self.powerup_sound.play()
+
+    def display_hearts(self):
+        for i in range(self.player.hearts):
+            x = i * (self.heart_surface.get_width() + 2) + 2
+            self.display_surface.blit(self.heart_surface, (x, 4))
+
+    def create_bg(self):
+        bg_original = pygame.image.load("graphics/other/bg.png").convert()
+        scale_factor = WINDOW_HEIGHT / bg_original.get_height()
+        w = bg_original.get_width() * scale_factor
+        h = bg_original.get_height() * scale_factor
+        scaled_bg = pygame.transform.scale(bg_original, (w, h))
+        return scaled_bg
+
+    def stage_setup(self):
+        for row_index, row in enumerate(BLOCK_MAP):
+            y = row_index * (BLOCK_HEIGHT + GAP_SIZE) + GAP_SIZE // 2 + TOP_OFFSET
+            for col_index, col in enumerate(row):
+                x = col_index * (BLOCK_WIDTH + GAP_SIZE) + GAP_SIZE // 2
+                if col != " ":
+                    Block(
+                        [self.all_sprites, self.block_sprites],
+                        col,
+                        (x, y),
+                        self.create_upgrade,
+                    )
+
+    def run(self):
+        last_time = time.time()
+        while True:
+
+            # delta time
+            dt = time.time() - last_time
+            last_time = time.time()
+
+            # event loop
+            for event in pygame.event.get([pygame.QUIT, pygame.KEYDOWN]):
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        if self.can_shoot:
+                            self.create_projectile()
+                            self.can_shoot = False
+                            self.shoot_time = pygame.time.get_ticks()
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+
+            if self.player.hearts <= 0:
+                pygame.quit()
+                sys.exit()
+
+            # draw background
+            self.display_surface.blit(self.bg, (0, 0))
+
+            # update the game
+            self.all_sprites.update(dt)
+            self.upgrade_collisions()
+            self.laser_timer()
+            self.projectile_collisions()
+
+            # update UI
+            self.all_sprites.draw(self.display_surface)
+            self.display_hearts()
+            self.crt.draw()
+
+            # update window
+            pygame.display.update()
+            self.clock.tick(FRAMERATE)
+
+
+class CRT:
+    """Simulates old TV"""
+
+    def __init__(self):
+        vignette = pygame.image.load("graphics/other/tv.png").convert_alpha()
+        self.scaled_vignette = pygame.transform.scale(
+            vignette, (WINDOW_WIDTH, WINDOW_HEIGHT)
+        )
+        self.display_surface = pygame.display.get_surface()
+        self.create_crt_lines()
+
+    def create_crt_lines(self):
+        line_height = 4
+        line_amount = WINDOW_HEIGHT // line_height
+        for line in range(line_amount):
+            y = line * line_height
+            pygame.draw.line(
+                self.scaled_vignette, (20, 20, 20), (0, y), (WINDOW_WIDTH, y), 1
+            )
+
+    def draw(self):
+        self.scaled_vignette.set_alpha(random.randint(50, 90))
+        self.display_surface.blit(self.scaled_vignette, (0, 0))
+
+
+def main():
+    game = Game()
+    game.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ code/sprites.py
@@ -0,0 +1,324 @@
+import random
+from typing import Callable, List, Tuple
+import pygame
+from surface_maker import SurfaceMaker
+from settings import (
+    BLOCK_HEIGHT,
+    BLOCK_WIDTH,
+    COLOR_LEGEND,
+    WINDOW_WIDTH,
+    WINDOW_HEIGHT,
+)
+
+
+class GameObject(pygame.sprite.Sprite):
+    def __init__(self, groups: List[pygame.sprite.Group]):
+        super().__init__(groups)
+        self.image = pygame.Surface((0, 0))
+        self.rect = self.image.get_rect()
+        self.old_rect = self.rect.copy()
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2()
+        self.speed = 0
+
+    def input(self):
+        pass
+
+    def get_damage(self, amount: int):
+        pass
+
+
+class Player(GameObject):
+    def __init__(self, groups: List[pygame.sprite.Group]):
+        super().__init__(groups)
+
+        # setup
+        self.display_surface = pygame.display.get_surface()
+        self.image = SurfaceMaker.get_surf(
+            "player", (WINDOW_WIDTH // 10, WINDOW_HEIGHT // 20)
+        )
+        self.hearts = 3
+        self.laser_amount = 0
+        self.laser_surface = pygame.image.load(
+            "graphics/other/laser.png"
+        ).convert_alpha()
+        self.laser_rects: List[pygame.Rect] = []
+
+        # position
+        self.rect = self.image.get_rect(
+            midbottom=(WINDOW_WIDTH // 2, WINDOW_HEIGHT - 20)
+        )
+        self.old_rect = self.rect.copy()
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.speed = 300
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def upgrade(self, upgrade_type: str):
+        if upgrade_type == "speed":
+            self.speed += 50
+        elif upgrade_type == "laser":
+            self.laser_amount += 1
+        elif upgrade_type == "heart":
+            self.hearts += 1
+        elif upgrade_type == "size":
+            new_width = self.rect.width * 1.1
+            self.image = SurfaceMaker.get_surf("player", (new_width, self.rect.height))
+            self.rect = self.image.get_rect(center=self.rect.center)
+            self.pos.x = self.rect.x
+
+    def display_lasers(self):
+        self.laser_rects = []
+        if self.laser_amount > 0:
+            divider_length = self.rect.width / (self.laser_amount + 1)
+            for i in range(self.laser_amount):
+                x = self.rect.left + divider_length * (i + 1)
+                laser_rect = self.laser_surface.get_rect(midbottom=(x, self.rect.top))
+                self.laser_rects.append(laser_rect)
+                self.display_surface.blit(self.laser_surface, laser_rect)
+
+    def screen_constraint(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        elif self.rect.right >= WINDOW_WIDTH:
+            self.rect.right = WINDOW_WIDTH
+        self.pos.x = self.rect.x
+
+    def update(self, dt: float):
+        self.input()
+
+        # copy rect before move it
+        self.old_rect = self.rect.copy()
+
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.x = round(self.pos.x)
+        self.screen_constraint()
+        self.display_lasers()
+
+
+class Ball(GameObject):
+    def __init__(
+        self,
+        groups: List[pygame.sprite.Group],
+        player: Player,
+        blocks: pygame.sprite.Group,
+    ):
+        super().__init__(groups)
+
+        # collisions
+        self.player = player
+        self.blocks = blocks
+
+        # setup
+        self.image = pygame.image.load("graphics/other/ball.png").convert_alpha()
+
+        # position
+        self.rect = self.image.get_rect(midbottom=self.player.rect.midbottom)
+        self.old_rect = self.rect.copy()
+        self.direction = pygame.math.Vector2((random.choice((1, -1)), -1))
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.speed = 400
+
+        # active
+        self.active = False
+
+        # sounds
+        self.impact_sound = pygame.mixer.Sound("sounds/impact.wav")
+        self.impact_sound.set_volume(0.1)
+        self.fail_sound = pygame.mixer.Sound("sounds/fail.wav")
+        self.fail_sound.set_volume(0.1)
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.midbottom == self.player.rect.midtop:
+            self.active = True
+
+    def window_collision(self, direction: str):
+        if direction == "horizontal":
+            if self.rect.left <= 0:
+                self.rect.left = 0
+                self.pos.x = self.rect.x
+                self.direction.x *= -1
+            elif self.rect.right >= WINDOW_WIDTH:
+                self.rect.right = WINDOW_WIDTH
+                self.pos.x = self.rect.x
+                self.direction.x *= -1
+        if direction == "vertical":
+            if self.rect.top <= 0:
+                self.rect.top = 0
+                self.pos.y = self.rect.y
+                self.direction.y *= -1
+            elif self.rect.top >= WINDOW_HEIGHT:
+                self.active = False
+                self.direction.y = -1
+                self.player.hearts -= 1
+                self.fail_sound.play()
+
+    def collision(self, direction: str):
+
+        # find overlapping objects
+        overlap_sprites: List[GameObject] = pygame.sprite.spritecollide(
+            self, self.blocks, False
+        )
+        if self.rect.colliderect(self.player.rect):
+            overlap_sprites.append(self.player)
+
+        if overlap_sprites:
+            if direction == "horizontal":
+                for sprite in overlap_sprites:
+                    if (
+                        self.rect.right >= sprite.rect.left
+                        and self.old_rect.right <= sprite.old_rect.left
+                    ):
+                        self.rect.right = sprite.rect.left
+                        self.pos.x = self.rect.x
+                        self.direction.x *= -1
+                        sprite.get_damage(1)
+                        self.impact_sound.play()
+                        break
+                    elif (
+                        self.rect.left <= sprite.rect.right
+                        and self.old_rect.left >= sprite.old_rect.right
+                    ):
+                        self.rect.left = sprite.rect.right
+                        self.pos.x = self.rect.x
+                        self.direction.x *= -1
+                        sprite.get_damage(1)
+                        self.impact_sound.play()
+                        break
+            elif direction == "vertical":
+                for sprite in overlap_sprites:
+                    if (
+                        self.rect.bottom >= sprite.rect.top
+                        and self.old_rect.bottom <= sprite.old_rect.top
+                    ):
+                        self.rect.bottom = sprite.rect.top
+                        self.pos.y = self.rect.y
+                        self.direction.y *= -1
+                        sprite.get_damage(1)
+                        self.impact_sound.play()
+                        break
+                    elif (
+                        self.rect.top <= sprite.rect.bottom
+                        and self.old_rect.top >= sprite.old_rect.bottom
+                    ):
+                        self.rect.top = sprite.rect.bottom
+                        self.pos.y = self.rect.y
+                        self.direction.y *= -1
+                        sprite.get_damage(1)
+                        self.impact_sound.play()
+                        break
+
+    def update(self, dt: float):
+        self.input()
+
+        if self.active:
+            if self.direction.magnitude() != 0:
+                self.direction = self.direction.normalize()
+
+            # copy rect before move it
+            self.old_rect = self.rect.copy()
+
+            # horizontal collistions
+            self.pos.x += self.direction.x * self.speed * dt
+            self.rect.x = self.pos.x
+            self.collision("horizontal")
+            self.window_collision("horizontal")
+
+            # vertical collistion
+            self.pos.y += self.direction.y * self.speed * dt
+            self.rect.y = self.pos.y
+            self.collision("vertical")
+            self.window_collision("vertical")
+        else:
+            # whenever the ball becomes inactive, it comes back to the player
+            self.rect.midbottom = self.player.rect.midtop
+            self.pos = pygame.math.Vector2(self.rect.topleft)
+
+
+class Block(GameObject):
+    def __init__(
+        self,
+        groups: List[pygame.sprite.Group],
+        block_type: str,
+        pos: Tuple[int, int],
+        create_upgrade: Callable[[Tuple[int, int]], None],
+    ):
+        super().__init__(groups)
+
+        # setup
+        self.image = SurfaceMaker.get_surf(
+            COLOR_LEGEND[block_type], (BLOCK_WIDTH, BLOCK_HEIGHT)
+        )
+        self.rect = self.image.get_rect(topleft=pos)
+        self.old_rect = self.rect.copy()
+
+        # damage information
+        self.health = int(block_type)
+
+        # upgrade
+        self.create_upgrade = create_upgrade
+
+    def get_damage(self, amount: int):
+        self.health -= amount
+        if self.health > 0:
+            # update the image
+            self.image = SurfaceMaker.get_surf(
+                COLOR_LEGEND[str(self.health)], (BLOCK_WIDTH, BLOCK_HEIGHT)
+            )
+        else:
+            if random.randint(0, 10) < 3:
+                self.create_upgrade(self.rect.center)
+            self.kill()
+
+
+class Upgrade(pygame.sprite.Sprite):
+    def __init__(
+        self, groups: List[pygame.sprite.Group], upgrade_type: str, pos: Tuple[int, int]
+    ):
+        super().__init__(groups)
+
+        self.upgrade_type = upgrade_type
+        self.image = pygame.image.load(
+            f"graphics/upgrades/{self.upgrade_type}.png"
+        ).convert_alpha()
+        self.rect = self.image.get_rect(midtop=pos)
+
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.speed = 300
+
+    def update(self, dt: float):
+        self.pos.y += self.speed * dt
+        self.rect.y = round(self.pos.y)
+
+        if self.rect.top > WINDOW_HEIGHT + 100:
+            self.kill()
+
+
+class Projectile(pygame.sprite.Sprite):
+    def __init__(
+        self,
+        groups: List[pygame.sprite.Group],
+        pos: Tuple[int, int],
+        surface: pygame.Surface,
+    ):
+        super().__init__(groups)
+        self.image = surface
+        self.rect = self.image.get_rect(midbottom=pos)
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.speed = 300
+
+    def update(self, dt: float):
+        self.pos.y -= self.speed * dt
+        self.rect.y = round(self.pos.y)
+
+        if self.rect.bottom <= -100:  # passed the top of screen
+            self.kill()
